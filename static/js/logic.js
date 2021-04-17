0aa2dc6a70013fad64776106d0fad9bfa219ff39
Ciara Spencer <ciara.spencer314@gmail.com>
2021-04-16 22:16:49 -0400

    leaflet-1

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,18 +9,28 @@ d3.json(queryUrl, function(data) {
 });
 
 function createFeatures(earthquakeData) {
+    
+    function onEachLayer(feature) {
+        return new L.circleMarker([feature.geometry.coordinates[1], feature.geometry.coordinates[0]], {
+          radius: getRadius(feature.properties.mag),
+          fillOpacity: 0.8,
+          color: getColor(feature.properties.mag),
+          fillColor: getColor(feature.properties.mag)
+        });
+    }
 
   // Define a function we want to run once for each feature in the features array
   // Give each feature a popup describing the place and time of the earthquake
-  function onEachFeature(feature, layer) {
-    layer.bindPopup("<h3>" + feature.properties.place +
-      "</h3><hr><p>" + new Date(feature.properties.time) + "</p>");
-  }
+    function onEachFeature(feature, layer) {
+        layer.bindPopup("<h3>" + feature.properties.place +
+        "</h3><hr><p>" + new Date(feature.properties.time) + "</p><hr><p>" + feature.properties.mag + "</p>");
+    }
 
   // Create a GeoJSON layer containing the features array on the earthquakeData object
   // Run the onEachFeature function once for each piece of data in the array
   var earthquakes = L.geoJSON(earthquakeData, {
-    onEachFeature: onEachFeature
+    onEachFeature: onEachFeature,
+    pointToLayer: onEachLayer
   });
 
   // Sending our earthquakes layer to the createMap function
@@ -30,14 +40,14 @@ function createFeatures(earthquakeData) {
 function createMap(earthquakes) {
 
   // Define streetmap and darkmap layers
-  var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
-    tileSize: 512,
-    maxZoom: 18,
-    zoomOffset: -1,
-    id: "mapbox/streets-v11",
-    accessToken: API_KEY
-  });
+//   var satmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+//     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+//     tileSize: 512,
+//     maxZoom: 18,
+//     zoomOffset: -1,
+//     id: "mapbox.satellite",
+//     accessToken: API_KEY
+//   });
 
   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
@@ -48,7 +58,7 @@ function createMap(earthquakes) {
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
-    "Street Map": streetmap,
+    // "Satellite Map": satmap,
     "Dark Map": darkmap
   };
 
@@ -63,7 +73,7 @@ function createMap(earthquakes) {
       37.09, -95.71
     ],
     zoom: 5,
-    layers: [streetmap, earthquakes]
+    layers: [darkmap, earthquakes]
   });
 
   // Create a layer control
@@ -73,3 +83,29 @@ function createMap(earthquakes) {
     collapsed: false
   }).addTo(myMap);
 }
+
+function getColor(magnitude) {
+    // Conditionals for magnitude
+    if (magnitude >= 5) {
+      return "#FF5F65";
+    }
+    else if (magnitude >= 4) {
+      return "#FCA35D";
+    }
+    else if (magnitude >= 3) {
+     return "#FDB72A";
+    }
+    else if (magnitude >= 2) {
+      return "#F7DB11";
+    }
+    else if (magnitude >= 1) {
+      return "#DCF400";
+    }
+    else {
+      return "#A3F600";
+    }
+};
+
+function getRadius(magnitude) {
+    return magnitude ** 2;
+  }
