3425df2a84fc961c3ede47e49b9f18bd1f54d5bd
Ciara Spencer <ciara.spencer314@gmail.com>
2021-10-04 19:09:10 -0400

    file update

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/", endpoint='index2')
+
+@app.route("/", endpoint='index')
+def home():
+    return render_template("index.html")
+
+@app.route("/7days", endpoint='index2')
 def home():
     return render_template("index2.html")
 
